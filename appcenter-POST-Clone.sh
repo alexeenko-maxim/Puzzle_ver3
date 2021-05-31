95240f41e327e83e6a56a3f5d0990e84803ea519
max <alekseenko@firecode.ru>
2021-05-31 15:13:53 +0300

    init

---
+++ appcenter-POST-Clone.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Example: Clone a required repository
+git clone https://github.com/alexeenko-maxim/Puzzle_ver3
+
+# Example: Install App Center CLI
+npm install -g appcenter-cli
